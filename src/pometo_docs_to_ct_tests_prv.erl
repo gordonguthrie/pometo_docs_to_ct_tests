991cb45a9c00eb5b2e44b645caf028508ae9cb72
Gordon Guthrie <gordon@hypernumbers.com>
2021-08-10 19:51:57 +0200

    remove silly error

--- src/pometo_docs_to_ct_tests_prv.erl
+++ src/pometo_docs_to_ct_tests_prv.erl
@@ -228,7 +228,7 @@ make_test(Title, Type, Seq, Code, Results) ->
 		"compiler_indexed" ->
 			"    Got = pometo_test_helper:run_" ++ Type ++ "_test(\"" ++ NameRoot ++ "\", Code),\n"
 		end,
-	Printing = "    ?io:format(\" in " ++ NameRoot ++ "(" ++ Type ++ ")~nCode:~n~ts~nExp:~n~ts~nGot:~n~ts~n\", [Code, Expected, Got]),\n",
+	Printing = "    io:format(\" in " ++ NameRoot ++ "(" ++ Type ++ ")~nCode:~n~ts~nExp:~n~ts~nGot:~n~ts~n\", [Code, Expected, Got]),\n",
 	Assert   = "    ?assertEqual(Expected, Got).\n\n",
 	{NameRoot, Main ++ Call ++ Printing ++ Assert}.
 
